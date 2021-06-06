abf9bf3d59e278a5cabb068750e2d35f5e0f30e9
Nathan <nathan.vmail@gmail.com>
2021-06-06 18:02:05 +0300

    do face recognize function

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def train(face_id, video):
     return count
 
 
-def face_recgonize():
+def face_recgonize(face_id):
     recognizer = cv2.face.LBPHFaceRecognizer_create()
     recognizer.read('trainer/trainer.yml')
     faceCascade = cv2.CascadeClassifier(cascade_path);
@@ -100,7 +100,14 @@ def face_recgonize():
     minW = 0.1 * cam.get(3)
     minH = 0.1 * cam.get(4)
 
-    while True:
+    count = 0
+    recognized = False
+
+    while not recognized:
+        print(count)
+        count += 1
+        if count > 100:
+            break
 
         ret, img = cam.read()
         # img = cv2.flip(img, -1)  # Flip vertically
@@ -120,6 +127,10 @@ def face_recgonize():
 
             id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 
+            if id == int(face_id):
+                recognized = True
+                break
+
             # Check if confidence is less them 100 ==> "0" is perfect match
             if (confidence < 100):
                 id = names[id]
@@ -131,25 +142,30 @@ def face_recgonize():
             cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
             cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
 
+
         cv2.imshow('camera', img)
 
         k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
         if k == 27:
             break
 
+
     # Do a bit of cleanup
     print("\n [INFO] Exiting Program and cleanup stuff")
     cam.release()
     cv2.destroyAllWindows()
 
+    return recognized
+
 
 def main():
     face_id = input('\n enter user id and press <return> ==>  ')
     # video = input('\n enter video path and press <return> ==>  ')
-    video = 'nathan.mp4'
-    count = train(face_id, video)
-    print("\nDetector captured " + str(count) + " faces")
-    face_recgonize()
+    # video = 'nathan.mp4'
+    # count = train(face_id, video)
+    # print("\nDetector captured " + str(count) + " faces")
+    is_rec = face_recgonize(face_id)
+    print(is_rec)
 
 
 if __name__ == "__main__":
