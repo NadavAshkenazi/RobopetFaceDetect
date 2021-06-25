439d2bf99bfed13a8d585f948f326dd5e7d1a32a
NadavTAshkenazi <nadavash@campus.technion.ac.il>
2021-06-25 14:27:20 +0300

    add getLocation

--- main.py
+++ main.py
@@ -157,6 +157,43 @@ def face_recgonize(face_id):
 
     return recognized
 
+def getLocation():
+    faceCascade = cv2.CascadeClassifier(cascade_path)
+    # Initialize and start realtime video capture
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640)  # set video widht
+    cam.set(4, 480)  # set video height
+
+    # Define min window size to be recognized as a face
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
+
+
+    while True:
+        ret, img = cam.read()
+        # img = cv2.flip(img, -1)  # Flip vertically
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(int(minW), int(minH)),
+        )
+
+        h_i, w_i, c = img.shape
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            return (x + w / 2) / h_i, (y + h / 2) / w_i
+
+        cv2.imshow('camera', img)
+
+        k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
+        if k == 27:
+            break
+
 
 def main():
     face_id = input('\n enter user id and press <return> ==>  ')
