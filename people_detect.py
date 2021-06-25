0186418e99a49da26a76dde6c1d12dbc9089e20e
Nathan <nathan.vmail@gmail.com>
2021-06-25 13:19:47 +0300

    add people and object detect

---
+++ people_detect.py
@@ -0,0 +1,44 @@
+# import the necessary packages
+from __future__ import print_function
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+while True:
+    ret, img = cap.read()
+    image = imutils.resize(img, width=min(400, img.shape[1]))
+    orig = image.copy()
+    # detect people in the image
+    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+                                            padding=(16, 16), scale=1.02)
+    # draw the original bounding boxes
+    for (x, y, w, h) in rects:
+        cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    # apply non-maxima suppression to the bounding boxes using a
+    # fairly large overlap threshold to try to maintain overlapping
+    # boxes that are still people
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    # draw the final bounding boxes
+    for (xA, yA, xB, yB) in pick:
+        cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+    # show the output images
+    # cv2.imshow("Before NMS", orig)
+    cv2.imshow("After NMS", image)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
+cap.release()
+cv2.destroyAllWindows()
+
